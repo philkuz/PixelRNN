33d8ac1819fd4193b0b844fb297aeb47ee33d998
Noah Golmant <noah.golmant@gmail.com>
2017-01-10 18:54:35 -0800

    add most of basic 1-channel convnet

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 from mnist import load_mnist
 from cifar10 import load_cifar
+import numpy as np
+
+def binarize(images):
+    rand = np.random.uniform(size=images.shape)
+    return (rand < images).astype('float32')
+    #return (images < 0.5).astype('float32')
+
 
 # TEST
 if __name__=="__main__":
